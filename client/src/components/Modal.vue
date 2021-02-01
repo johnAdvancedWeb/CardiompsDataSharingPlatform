e2408b7e1a56f54d2180eeb95203ac3b573cbbe4
Ibbi Alshamary <alshamaryi17@gmail.com>
2021-02-01 23:51:51 +0000

    Added modal to homepage route

---
+++ client/src/components/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="modal-mask" v-if="isModalVisible" @click="closeModal">
+    <button :plain="true" disabled>
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: ["isModalVisible", "actionLoginOrRegister"],
+  setup(props, context) {
+    function closeModal() {
+      context.emit("close-modal");
+    }
+
+    return { closeModal }
+  },
+}
+</script>
+
+<style scoped>
+.modal-mask {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.3);
+}
+
+button {
+  width: 80%;
+  height: auto;
+  margin: 50px auto 0;
+  position: relative;
+  padding: 16px;
+  background-color: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.13);
+  box-sizing: border-box;
+}
+
+</style>
---
+++ client/src/components/TestComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script>
+export default {
+name: "TestComponent"
+}
+</script>
+
+<style scoped>
+div {
+}
+</style>
