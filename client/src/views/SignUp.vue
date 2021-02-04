f4f456f1241b27114c3b3e292df1509c31079056
Ibbi Alshamary <alshamaryi17@gmail.com>
2021-02-04 09:05:32 +0000

    sign-up form front-end implemented

--- client/src/views/SignUp.vue
+++ client/src/views/SignUp.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    <p>Welcome to the sign up page</p>
+  <div id="register-container">
+    <div id="register-header">Sign up below</div>
+    <div id="register-form">
+      <form>
+        <div id="username-container">
+          <label for="email">Email:</label><br>
+          <input type="text" placeholder="Enter email here" id="email"><br>
+        </div>
+
+        <div id="full-name-container">
+          <label for="full-name">Full Name:</label><br>
+          <input type="text" placeholder="Enter full name here" id="full-name"><br>
+        </div>
+
+        <div id="password-container">
+          <label for="password">Password:</label><br>
+          <input type="password" placeholder="Enter password here" id="password"><br>
+        </div>
+
+        <div id="confirm-password-container">
+          <label for="confirm-password">Confirm Password:</label><br>
+          <input type="password" placeholder="Confirm password here" id="confirm-password"><br>
+        </div>
+
+        <button>Sign up</button>
+      </form>
+    </div>
   </div>
 </template>
 
