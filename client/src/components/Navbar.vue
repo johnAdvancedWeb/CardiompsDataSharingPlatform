0fbbf56bfdd0d39f76bda847e9d9799ace433784
unknown <mr.j.j.cunningham@gmail.com>
2021-02-06 22:24:25 +0000

    Ibrahim's nav bar is commented out

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <nav class="flex justify-between">
       <div>
         <ul class="flex flex-row">
-          <li class="navbar-list-items-logo"><img src="../assets/images/Logo.gif" alt="Beating Heart Logo" id="logo-gif"></li>
+          <img src="../assets/images/Logo.gif" alt="Beating Heart Logo" id="logo-gif">
           <li class="navbar-list-items-button"><button id="logo">Cardiomps DSP</button></li>
           <li class="navbar-list-items-routes"><router-link to="/">Home</router-link></li>
           <li class="navbar-list-items-routes"><router-link :to="{ name: 'Line' }">Add Experimental Data</router-link></li>
@@ -20,6 +20,33 @@
       </div>
     </nav>
   </div>
+
+<!-- Ibrahim's nav bar-->
+<!--<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <a class="navbar-brand" href="#">Navbar</a>
+
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>-->
+
 </template>
 
 <script>
