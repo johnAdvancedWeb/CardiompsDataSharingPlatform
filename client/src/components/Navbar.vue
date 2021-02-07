5b9ad6ab2db8d94a426615a6325a48f8ecac7899
Ibbi Alshamary <alshamaryi17@gmail.com>
2021-02-07 15:38:07 +0000

    navbar fully responsive and colours matching well

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,61 +1,43 @@
 <template>
-<!--  <nav class="navbar navbar-expand-lg" id="navbar">-->
-<!--    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03"-->
-<!--            aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">-->
-<!--      <span class="navbar-toggler-icon"></span>-->
-<!--    </button>-->
-<!--    <a class="navbar-brand" href="#" id="logo">Cardiomps DSP</a>-->
-
-<!--    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">-->
-<!--      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">-->
-
-<!--        <li class="navbar-list-items-routes">-->
-<!--          <router-link class="nav-link" to="/">Home</router-link>-->
-<!--        </li>-->
-
-<!--        <li class="navbar-list-items-routes">-->
-<!--          <router-link class="nav-link" :to="{ name: 'Line' }">Add Experimental Data</router-link>-->
-<!--        </li>-->
-
-<!--        <li class="navbar-list-items-routes">-->
-<!--          <router-link class="nav-link" :to="{ name: 'Bar' }">View Experimental Data</router-link>-->
-<!--        </li>-->
-
-<!--        <li class="navbar-list-items-routes">-->
-<!--          <router-link class="nav-link" to="/about">Help and About</router-link>-->
-<!--        </li>-->
-
-<!--      </ul>-->
-
-<!--      <router-link class="button btn my-2 my-sm-0" style="margin-right: 5px" :to="{ name: 'SignUp' }">Sign Up</router-link>-->
-<!--      <router-link class="button btn my-2 my-sm-0" style="margin-right: 5px" :to="{ name: 'SignIn' }">Sign In</router-link>-->
-<!--    </div>-->
-<!--  </nav>-->
-
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+  <nav class="navbar navbar-expand-lg" id="navbar">
+    <button class="navbar-toggler custom-toggler" type="button" data-toggle="collapse"
+            data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false"
+            aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon">
+        <i class="fas fa-bars custom-toggle-icon"></i>
+      </span>
     </button>
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#" id="logo">Cardiomps DSP</a>
 
     <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+
+        <li class="navbar-list-items-routes">
+          <router-link class="nav-link" to="/">Home</router-link>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+
+        <li class="navbar-list-items-routes">
+          <router-link class="nav-link" :to="{ name: 'Line' }">Add Experimental Data</router-link>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
+
+        <li class="navbar-list-items-routes">
+          <router-link class="nav-link" :to="{ name: 'Bar' }">View Experimental Data</router-link>
         </li>
+
+        <li class="navbar-list-items-routes">
+          <router-link class="nav-link" to="/about">Help and About</router-link>
+        </li>
+
       </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
+
+      <router-link class="button btn my-2 my-sm-0" style="margin-right: 5px" :to="{ name: 'SignUp' }">Sign Up
+      </router-link>
+      <router-link class="button btn my-2 my-sm-0" style="margin-right: 5px" :to="{ name: 'SignIn' }">Sign In
+      </router-link>
     </div>
   </nav>
+
+
 </template>
 
 <script>
