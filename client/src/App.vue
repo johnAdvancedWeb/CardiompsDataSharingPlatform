424b35feaec23cdf47b40a6f3676a70a7cd61e07
unknown <mr.j.j.cunningham@gmail.com>
2021-02-16 20:27:37 +0000

    The Sign In page script has been implemented

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,14 +1,34 @@
 <template>
-  <Navbar/>
-  <router-view/>
+  <div>
+    <Navbar/>
+    <router-view/>
+  </div>
 </template>
 
 <script>
+//import { ref } from "vue";
+//import { firebaseAuthentication } from "@/firebase/database";
 import Navbar from "@/components/Navbar";
 
 export default {
-  components: { Navbar }
-}
+  components: { Navbar },
+  
+  /*setup() {
+    const user = ref(null);
+
+    firebaseAuthentication.onAuthStateChanged( (currentUser) => {
+      if(currentUser) {
+        user.value = currentUser.email;
+      }
+      else {
+        user.value == null;
+      }
+    });
+
+    return { user };
+  }*/
+};
 </script>
+
 <style>
 </style>
--- client/src/views/SignIn.vue
+++ client/src/views/SignIn.vue
@@ -1,46 +1,58 @@
 <template>
-  <div id="login-container">
-    <div id="login-header">Sign in below</div>
-    <div id="login-form">
-      <form>
-        <div id="username-container">
-          <label for="email">Email:</label><br>
-          <input type="text" placeholder="Enter email here" id="email"><br>
-        </div>
+  <div>
 
-        <div id="password-container">
-          <label for="password">Password:</label><br>
-          <input type="password" placeholder="Enter password here" id="password"><br>
-          <a href="#" @click="showModal('resetPassword')">Forgotten password</a>
-        </div>
+    <div id="login-container">
+      <div id="login-header">Sign in below</div>
+      <div id="login-form">
+        <form @submit.prevent>
+          <div id="username-container">
+            <label for="email">Email:</label><br>
+            <input type="email" placeholder="Enter email here" id="email" v-model="email" required><br>
+          </div>
 
-        <button>Sign in</button>
-      </form>
+          <div id="password-container">
+            <label for="password">Password:</label><br>
+            <input type="password" placeholder="Enter password here" id="password" show-password v-model="password" required><br>
+            <a href="#" @click="showModal('resetPassword')">Forgotten password</a>
+          </div>
+
+          <div id="error-container" v-if="errorFirebase">
+            <p class="red-text">{{ errorFirebase }}</p>
+          </div>
+
+          <button plain type="success" @click="signIn">Sign in</button>
+        </form>
+      </div>
     </div>
-  </div>
+    
+    <Modal :is-modal-visible="isModalVisible" @close-modal="closeModal">
+      <ResetPassword v-if="actionDescription === 'resetPassword'"></ResetPassword>
+    </Modal>
 
-  <Modal :is-modal-visible="isModalVisible" @close-modal="closeModal">
-    <ResetPassword v-if="actionDescription === 'resetPassword'"></ResetPassword>
-    <TestComponent v-else></TestComponent>
-  </Modal>
+  </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import { firebaseAuthentication } from "@/firebase/database";
+import { useRouter } from "vue-router";
 import Modal from "@/components/Modal";
 import ResetPassword from "@/components/ResetPassword";
-import TestComponent from "@/components/TestComponent";
 
 export default {
-  name: 'SignIn',
+  name: "SignIn",
+
   components: {
-    Modal, ResetPassword, TestComponent
+    Modal, ResetPassword
   },
+  
   data() {
     return {
       isModalVisible: false,
       actionDescription: "",
     }
   },
+
   methods: {
     showModal(action) {
       this.actionDescription = action;
@@ -50,8 +62,34 @@ export default {
       this.isModalVisible = false;
     }
   },
+
+  setup() {
+    const email = ref("");
+    const password = ref("");
+    const errorFirebase = ref(null);
+    
+    const router = useRouter();
+    
+    function signIn() {
+      const info = {
+        email: email.value,
+        password: password.value,
+      };
+      
+      firebaseAuthentication.signInWithEmailAndPassword(info.email, info.password).then(
+          () => {
+            router.push("/");
+          },
+          (error) => {
+            errorFirebase.value = error.message;
+          }
+        );
+    }
+    return { email, password, errorFirebase, signIn };
+  },
 }
 </script>
+
 
 <style scoped>
 
