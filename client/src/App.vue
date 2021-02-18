ca76d1f663a16aee6cd30eb827c02796ca000842
Ibbi Alshamary <alshamaryi17@gmail.com>
2021-02-18 07:52:12 +0000

    able to create posts

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,65 @@
 <template>
   <Navbar/>
-  <router-view/>
+  <router-view @add-post="addPost"/>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar";
+import { firebaseFireStore } from "@/firebase/database";
 
 export default {
-  components: { Navbar }
+  components: {Navbar},
+  data() {
+    return {
+      posts: [
+        {
+          slug: "cardiomyopathyhcm",
+          title: "What are the symptoms of HCM?",
+          description: "Common symptoms of HCM",
+          content: `Shortness of breath, Chest pain, Palpitations, Light headedness and fainting.
+             You may find that you never have any serious problems related to your condition, and with treatment, your symptoms should be controlled.
+             However some people may find that their symptoms worsen or become harder to control in later life.
+
+             The area of heart muscle that is affected by HCM and the amount of stiffening that occurs will determine how the symptoms affect you.`,
+          tags: ["hcm", "hcm symptoms"],
+        },
+        {
+          slug: "cardiomyopathydcm",
+          title: "What are the symptoms of DCM?",
+          description: "Common symptoms of DCM",
+          content: `In most cases, DCM develops slowly, so some people can have quite severe symptoms before they are diagnosed. The most common symptoms are:
+            shortness of breath, swelling of the ankles and abdomen, excessive tiredness and palpitations.`,
+          tags: ["dcm", "dcm symptoms"],
+        },
+      ]
+    }
+  },
+
+  methods: {
+    addPost(slug, title, description, content, tags) {
+      const post = {
+        slug: slug,
+        title: title,
+        description: description,
+        content: content,
+        tags: tags
+      };
+
+      // const post = {
+      //   slug: "slug",
+      //   title: "title",
+      //   description: "description",
+      //   content: "content",
+      //   tags: "tags"
+      // };
+
+      firebaseFireStore
+          .collection("users")
+          .doc("dnlsk9Mskeg8DO1L1OQcHq3hnF92")
+          .collection("posts")
+          .add(post);
+    }
+  }
 }
 </script>
 <style>
--- client/src/views/AddExperimentalData.vue
+++ client/src/views/AddExperimentalData.vue
@@ -1,13 +1,81 @@
 <template>
-  <div>
-    <p>Add experimental data form goes here</p>
-  </div>
+
+  <ion-grid>
+    <ion-row>
+      <ion-col size-md="6" offset-md="3">
+        <div id="register-container">
+          <div id="register-header">Sign up below</div>
+          <div id="register-form">
+            <form @submit.prevent>
+              <div id="username-container">
+                <label for="slug">Slug: </label><br>
+                <input type="text" id="slug" v-model="slug" required><br>
+              </div>
+
+              <div id="full-name-container">
+                <label for="title">Title:</label><br>
+                <input type="text" id="title" v-model="title" required><br>
+              </div>
+
+              <div id="institution-container">
+                <label for="description">Description:</label><br>
+                <input type="text" id="description" v-model="description" required><br>
+              </div>
+
+              <div id="password-container">
+                <label for="content">Content: </label><br>
+                <input type="text" id="content" v-model="content" required><br>
+              </div>
+
+              <div id="confirm-password-container">
+                <label for="tags">Tags: </label><br>
+                <input type="text" id="tags" v-model="tags" required><br>
+              </div>
+
+              <button @click="addPost">Create Post</button><br>
+
+<!--              <transition name="fade-in">-->
+<!--                <div id="error-container" v-if="errorRegistration">-->
+<!--                  <p class="red-text">{{ errorRegistration }}</p>-->
+<!--                </div>-->
+<!--              </transition>-->
+            </form>
+          </div>
+        </div>
+      </ion-col>
+    </ion-row>
+  </ion-grid>
+
+
 </template>
 
 <script>
+
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 export default {
-  name: "AddExperimentalData"
-}
+  name: "AddExperimentalData",
+  emits: ["add-post"],
+  props: {
+    posts: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  setup(props, context) {
+    const slug = ref("");
+    const title = ref("");
+    const description = ref("");
+    const content = ref("");
+    const tags = ref("");
+    const router = useRouter();
+    function addPost() {
+      context.emit("add-post", slug.value, title.value, description.value, content.value, tags.value);
+      router.push("/");
+    }
+    return { slug, title, description, content, tags, addPost };
+  },
+};
 </script>
 
 <style scoped>
