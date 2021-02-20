99adeaf570d22d8535ec5466550a7ab5aa65e02d
Ibbi Alshamary <alshamaryi17@gmail.com>
2021-02-20 15:42:22 +0000

    My Posts added and delete post implemented

--- client/src/views/MyPosts.vue
+++ client/src/views/MyPosts.vue
@@ -1,17 +1,17 @@
 <template>
-
-
   <div id="section-one">
     <div class="min-h-screen p-10">
       <div class="space-y-10 md:space-y-0">
 
+        <!-- left -->
         <div class="md:flex md:flex-col md:justify-center">
           <h2 class="text-black text-2xl md:text-4xl font-bold mb-1">
-            <p>What's new</p>
+            <p>My Posts</p>
           </h2>
           <p class="text-sm md:text-lg mb-4">The latest cardiomyopathy news</p>
         </div>
 
+        <!-- right -->
         <div v-for="p in posts" :key="p.slug" style="padding: .5%">
           <div class="latest-news-container">
             <div class="container">
@@ -40,7 +40,7 @@
 
 
 export default {
-  name: 'Home',
+  name: 'MyPosts',
 
   components: {},
 
