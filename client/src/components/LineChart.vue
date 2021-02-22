eb4e3753f255c89ed3271f6327297e11dc8cd559
Ibbi Alshamary <alshamaryi17@gmail.com>
2021-02-22 09:14:06 +0000

    chart work

--- client/src/components/LineChart.vue
+++ client/src/components/LineChart.vue
@@ -1,89 +1,15 @@
 <template>
   <div>
-    <apexchart
-      type="line"
-      height="350"
-      width="500"
-      :options="chartOptions"
-      :series="series"
-    ></apexchart>
+    <br><br>
+    <MYBPC3_C1></MYBPC3_C1>
   </div>
-<!--  <button @click="test">Click me</button>-->
 
 </template>
 
 <script>
-import { ref } from "vue";
+import MYBPC3_C1 from "@/components/graph-groups/MYBPC3_C1";
 
 export default {
-  props: {
-    mutationGroups: {
-      type: Array,
-      default: () => [],
-    }
-  },
-
-  methods: {
-
-  },
-
-  setup() {
-    const chartOptions = ref({
-      chart: {
-        height: 350,
-        type: "line",
-        zoom: {
-          enabled: false,
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: "straight",
-      },
-      title: {
-        text: "Product Trends by Month",
-        align: "left",
-      },
-      grid: {
-        row: {
-          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-          opacity: 0.5,
-        },
-      },
-      xaxis: {
-        categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
-        title: {
-          text: "Month",
-        },
-      },
-      yaxis: {
-        title: {
-          text: "Desktops",
-        },
-      },
-      legend: {
-        position: "top",
-        horizontalAlign: "right",
-        floating: true,
-        offsetY: -25,
-        offsetX: -5,
-      },
-    });
-
-    const series = ref([
-      {
-        name: "Desktops",
-        data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
-      },
-      {
-        name: "Laptops",
-        data: [20, 61, 75, 71, 69, 72, 89, 111, 168],
-      },
-    ]);
-
-    return { chartOptions, series };
-  },
+  components: {MYBPC3_C1},
 };
 </script>
