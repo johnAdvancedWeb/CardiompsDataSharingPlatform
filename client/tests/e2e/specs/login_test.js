32a68df8a4ea0f732a1a08586d7083083733fba2
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2021-02-23 06:12:10 +0000

    amended login test

--- client/tests/e2e/specs/login_test.js
+++ client/tests/e2e/specs/login_test.js
@@ -11,21 +11,25 @@ describe('Login Page', () => {
   it("user can input into fields", ()=>{
     
     cy.get("#email").type("you33@firebase.com");
-    cy.get("#password").type("123456");
+    cy.get("#password").type("1234");
+
+  })
+
+  it("form vaildation", ()=>{
+    cy.get("#login-form > form > button").click();
+    cy.get("p.red-text").should("exist");
 
   })
 
   it("user can login", ()=> {
-    
+    cy.get("#password").type("56");
     cy.get("#login-form > form > button").click();
-
     cy.contains("What's new")
   })
 
   it("user can logout", ()=> {
     
     cy.get("#navbarTogglerDemo03 > div > a").click();
-
     cy.contains("Sign in below")
   })
 
