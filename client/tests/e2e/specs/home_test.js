bc68d49e1007b88c5b9dd02c82f9434400d60da7
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2021-02-23 08:22:04 +0000

    changed the titles of the home tests

--- client/tests/e2e/specs/home_test.js
+++ client/tests/e2e/specs/home_test.js
@@ -1,25 +1,25 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe('Home Page', () => {
-  it('Static content loads', () => {
+describe('Home Page : signed out', () => {
+  it('static content loads', () => {
     cy.visit('/')
     cy.contains('p', "What's new")
     cy.contains('p', "The latest cardiomyopathy news")
   })
 
-  it('Dynamic content loads', () => {
+  it('dynamic content loads', () => {
     cy.contains('p', "What's new");
     cy.get('div#post').should('not.exist');
 
   })
 
-  it('Sign up button changes route',() =>{
+  it('sign up button changes route',() =>{
     cy.get("#navbarTogglerDemo03 > div > a:nth-child(1)").click();
     cy.url().should('equal','http://localhost:8081/sign-up');
 
   })
 
-  it('Sign in button changes route',() =>{
+  it('sign in button changes route',() =>{
     cy.get("#navbarTogglerDemo03 > div > a:nth-child(2)").click();
     cy.url().should('equal','http://localhost:8081/sign-in');
   })
