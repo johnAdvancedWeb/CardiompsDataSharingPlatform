20fced7b25ed235ea78ea9e111ede5be58924c31
Mgm-source <55754333+Mgm-source@users.noreply.github.com>
2021-02-26 13:56:59 +0000

    update tesat

--- client/__tests__/home.test.js
+++ client/__tests__/home.test.js
@@ -5,16 +5,7 @@ describe("home test ",()=> {
 
     test("render content", () => {
         
-        const wrapper = mount(home,{
-            data() {
-                return {
-                isModalVisible: false,
-                actionDescription: "",
-                disease: [ { diseaseId : "test1", diseaseName : "test" },{diseaseId : "test2", diseaseName: "test"} ],
-                catTermMap: [ {catLabel: "test1", terms : [{name : "test ", definition : "testing" }]}, {catLabel: "test2", terms : [{name : "test ", definition : "testing" }],}]
-                }
-            }
-        })
+        const wrapper = mount(home);
     
         expect(wrapper.text()).toMatch("test1");
     })
